82a9f73801570cd25a04278ebb40350876738f42
siri5678 <sireeshapudi0221.com>
2026-03-08 16:31:03 -0400

    Added insertion sort program in decreasing order

--- insertion_sort.py
+++ insertion_sort.py
@@ -1,1 +1,21 @@
-print("hello world")
+def insertion_sort_desc(arr):
+    for j in range(1, len(arr)):
+        key = arr[j]
+        i = j - 1
+
+        while i >= 0 and arr[i] < key:
+            arr[i + 1] = arr[i]
+            i -= 1
+
+        arr[i + 1] = key
+
+    return arr
+
+
+numbers = [5, 2, 4, 6, 1, 3]
+
+print("Original array:", numbers)
+
+sorted_numbers = insertion_sort_desc(numbers)
+
+print("Sorted array in decreasing order:", sorted_numbers)
